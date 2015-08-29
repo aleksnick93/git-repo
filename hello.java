2a65ab392520490e23dd32191e052bc748253714
Alex Nick <aleksnick93@yandex.ru>
2015-08-29 12:14:43 +0300

    New feature

--- hello.java
+++ hello.java
@@ -2,3 +2,6 @@ System.out.println("Say hi to the humanity");
 System.out.println("Did you say hello to the world?");
 if( answer == 'no')
 	System.out.println("So close to fail);
+if (people.isInLove) {
+		people.setMotivation(getMotivation()++);
+}
